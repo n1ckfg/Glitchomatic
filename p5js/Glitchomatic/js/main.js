a23d39f7e17e3e9d60914bbae678a4f3350c4403
Nick Fox-Gieg <nick@fox-gieg.com>
2020-10-20 01:31:17 -0400

    test

--- p5js/Glitchomatic/js/main.js
+++ p5js/Glitchomatic/js/main.js
@@ -10,51 +10,51 @@ let snowFrameOdds = 0.5; //0 to 1
 let delayTime = 100;
 
 function setup() {
-  size(50,50);
-  Settings settings = new Settings("settings.txt");
-  //chooseFolderDialog();
-  loadFiles();
-  try {
-    tempImage = loadImage((String) imgNames.get(0));
-    surface.setSize(tempImage.width,tempImage.height);
-  } catch (e) {
-    console.log("No image files loaded. Exiting...");
-    exit();
-  }
+    size(50,50);
+    Settings settings = new Settings("settings.txt");
+    //chooseFolderDialog();
+    loadFiles();
+    try {
+        tempImage = loadImage((String) imgNames.get(0));
+        surface.setSize(tempImage.width,tempImage.height);
+    } catch (e) {
+        console.log("No image files loaded. Exiting...");
+        exit();
+    }
 }
 
 function draw() {
-  //create glitches
-  for (let i = 0; i<imgNames.size(); i++) {
-    byte[] data=loadBytes((String) imgNames.get(i));
-    for (let j=0;j<numChanges;j++) {
-      let loc=int(random(128,data.length));//guess at header being 128 bytes at most..
-      data[loc]=(byte)random(255);
+    //create glitches
+    for (let i = 0; i<imgNames.size(); i++) {
+        byte[] data=loadBytes((String) imgNames.get(i));
+        for (let j=0;j<numChanges;j++) {
+            let loc=int(random(128,data.length));//guess at header being 128 bytes at most..
+            data[loc]=(byte)random(255);
+        }
+        let target = targetDir + "/frame" + zeroPadding(i+1,imgNames.size()) + "." + fileType;
+        if (random(1)<snowFrameOdds) {
+            JpegMaker jpegMaker = new JpegMaker(width,height,target);
+        }else{
+            try{
+                saveBytes(target,data);
+                tempImage = loadImage(target);
+                image(tempImage,0,0);
+            }catch(e) {
+                console.log("error");
+            }
+        }
+        saveFrame(target); 
+        glitchCounterHandler();
+        console.log("output:    " + target);
     }
-    let target = targetDir + "/frame" + zeroPadding(i+1,imgNames.size()) + "." + fileType;
-    if (random(1)<snowFrameOdds) {
-      JpegMaker jpegMaker = new JpegMaker(width,height,target);
-    }else{
-      try{
-        saveBytes(target,data);
-        tempImage = loadImage(target);
-        image(tempImage,0,0);
-      }catch(e) {
-        console.log("error");
-      }
-    }
-    saveFrame(target); 
-    glitchCounterHandler();
-    console.log("output:  " + target);
-  }
-  exit();
+    exit();
 }
 
 function glitchCounterHandler() {
-  if (counter<counterMax) {
-    counter++;
-  } else if (counter>= counterMax) {
-    numChanges++;
-    counter=0;
-  }
+    if (counter<counterMax) {
+        counter++;
+    } else if (counter>= counterMax) {
+        numChanges++;
+        counter=0;
+    }
 }
