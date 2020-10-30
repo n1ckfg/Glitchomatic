829ff114f5331af57a8373f256898af5ed33f5c9
Nick Fox-Gieg <nick@fox-gieg.com>
2020-10-30 02:59:46 -0400

    continue p5js port

--- p5js/Glitchomatic/js/main.js
+++ p5js/Glitchomatic/js/main.js
@@ -9,45 +9,47 @@ let counterMax=100;
 let snowFrameOdds = 0.5; //0 to 1
 let delayTime = 100;
 
+function preload() {
+
+}
+
 function setup() {
-    size(50,50);
-    Settings settings = new Settings("settings.txt");
-    //chooseFolderDialog();
+    createCanvas(800, 600);
+
     loadFiles();
     try {
         tempImage = loadImage((String) imgNames.get(0));
         surface.setSize(tempImage.width,tempImage.height);
     } catch (e) {
-        console.log("No image files loaded. Exiting...");
-        exit();
+        console.log("No image files loaded.");
     }
 }
 
 function draw() {
     //create glitches
-    for (let i = 0; i<imgNames.size(); i++) {
+    for (let i = 0; i < imgNames.size(); i++) {
         byte[] data=loadBytes((String) imgNames.get(i));
-        for (let j=0;j<numChanges;j++) {
-            let loc=int(random(128,data.length));//guess at header being 128 bytes at most..
-            data[loc]=(byte)random(255);
+        for (let j = 0; j < numChanges; j++) {
+            let loc = parseInt(random(128, data.length));//guess at header being 128 bytes at most..
+            data[loc] = (byte) random(255);
         }
         let target = targetDir + "/frame" + zeroPadding(i+1,imgNames.size()) + "." + fileType;
-        if (random(1)<snowFrameOdds) {
+        if (random(1) < snowFrameOdds) {
             JpegMaker jpegMaker = new JpegMaker(width,height,target);
-        }else{
-            try{
-                saveBytes(target,data);
+        } else {
+            try {
+                saveBytes(target, data);
                 tempImage = loadImage(target);
                 image(tempImage,0,0);
-            }catch(e) {
+            } catch( e) {
                 console.log("error");
             }
         }
+        
         saveFrame(target); 
         glitchCounterHandler();
         console.log("output:    " + target);
     }
-    exit();
 }
 
 function glitchCounterHandler() {
