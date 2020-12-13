ed3126e2d9f5ff9f8c31c35e657637848bcab654
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-13 01:41:26 -0500

    test

--- p5js/Glitchomatic/js/main.js
+++ p5js/Glitchomatic/js/main.js
@@ -3,7 +3,7 @@
 const numChanges = 40; // default 100 changes
 let counter=0;
 let counterMax=100;
-let snowFrameOdds = 0.5; //0 to 1
+let snowFrameOdds = 0; //0 to 1
 let delayTime = 100;
 let armCanvasResize = false;
 let armGlitch = false;
@@ -20,36 +20,24 @@ function draw() {
     if (armDropResult) {
         armDropResult = false;
 
-        dropImage = loadImage(dropResult);
+        dropImage = loadImage(dropResult, function() {
+            resizeCanvas(dropImage.width, dropImage.height);
 
-        armCanvasResize = true;
-    }
-
-    if (armCanvasResize && dropImage.width !== 1 && dropImage.height !== 1) {
-        armCanvasResize = false;
-
-        resizeCanvas(dropImage.width, dropImage.height);
-
-        armGlitch = true;
-    }
-
-    if (armGlitch) {
-        if (random(1) < snowFrameOdds) {
-            dropImage = loadImage("data:image/jpeg;base64," + Uint8ToBase64(new JpegMaker(dropImage).bytes));
-        } else {
             dropBytes = loadBytes(dropResult, function(result) {
                 let data = result.bytes;
                 
-                for (let j = 0; j < numChanges; j++) {
-                    let loc = parseInt(random(128, data.length));//guess at header being 128 bytes at most..
-                    data[loc] = byte(parseInt(random(255)));
+                if (random(1) < snowFrameOdds) {
+                    dropImage = loadImage("data:image/jpeg;base64," + Uint8ToBase64(new JpegMaker(dropImage, data).bytes));
+                } else {
+                    for (let j = 0; j < numChanges; j++) {
+                        let loc = parseInt(random(128, data.length)); // guess at header being 128 bytes at most..
+                        data[loc] = byte(parseInt(random(255)));
+                    }
+                    
+                    dropImage = loadImage("data:image/jpeg;base64," + Uint8ToBase64(data));
                 }
-                
-                dropImage = loadImage("data:image/jpeg;base64," + Uint8ToBase64(data));
             });
-        }
-
-        armGlitch = false;
+        });
     }
 
     if (dropImage !== undefined) {
@@ -57,17 +45,17 @@ function draw() {
     }
 }
 
-    function Uint8ToBase64(bytes) {
-        let u8Arr = Uint8Array.from(bytes);
-        let CHUNK_SIZE = 0x8000; //arbitrary number
-        let index = 0;
-        let length = u8Arr.length;
-        let result = '';
-        let slice;
-        while (index < length) {
-            slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length)); 
-            result += String.fromCharCode.apply(null, slice);
-            index += CHUNK_SIZE;
-        }
-        return btoa(result);
+function Uint8ToBase64(bytes) {
+    let u8Arr = Uint8Array.from(bytes);
+    let CHUNK_SIZE = 0x8000; //arbitrary number
+    let index = 0;
+    let length = u8Arr.length;
+    let result = '';
+    let slice;
+    while (index < length) {
+        slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length)); 
+        result += String.fromCharCode.apply(null, slice);
+        index += CHUNK_SIZE;
     }
+    return btoa(result);
+}
